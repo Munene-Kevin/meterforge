cb1270e859fe370b74f5186cfa18847f14051e83
Munene-Kevin <neshdevlab@gmail.com>
2026-08-13 10:14:13 +0300

    chore: initialize MeterForge Spring Boot project

---
+++ src/main/java/com/meterforge/MeterForgeApplication.java
@@ -0,0 +1,12 @@
+package com.meterforge;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MeterForgeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MeterForgeApplication.class, args);
+    }
+}
